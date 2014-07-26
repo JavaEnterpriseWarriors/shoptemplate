7d1374436e0556c9fc9ee71dbfb8baab3fd8266a
JavaEnterpriseWarriors88 <jew88@spoko.pl>
2014-07-26 23:34:23 +0200

    Testy

---
+++ onionshop/src/main/java/pl/spoko/jew88/onionshop/ShopMain.java
@@ -0,0 +1,13 @@
+package pl.spoko.jew88.onionshop;
+
+public class ShopMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
