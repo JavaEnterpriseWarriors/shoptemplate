e361fda5f7bcfba3f571151a3c7819f0c4deb633
JavaEnterpriseWarriors <javaenterprisewarriors@gmail.com>
2014-08-12 15:51:27 +0200

    User fields added

--- main/src/main/java/pl/javaenterprisewarriors/shoptemplate/domain/User.java
+++ main/src/main/java/pl/javaenterprisewarriors/shoptemplate/domain/User.java
@@ -1,5 +1,31 @@
 package pl.javaenterprisewarriors.shoptemplate.domain;
 
+@Entity
+@Table(name = "USERS")
 public class User {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String country;
+	private String zipCode;
+	private String street;
+	private int phoneNumber;
+
+	public User() {
+	}
+
+	public User(String username, String password, String email, String country,
+			String zipCode, String street, int phoneNumber) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.country = country;
+		this.zipCode = zipCode;
+		this.street = street;
+		this.phoneNumber = phoneNumber;
+	}
 }
