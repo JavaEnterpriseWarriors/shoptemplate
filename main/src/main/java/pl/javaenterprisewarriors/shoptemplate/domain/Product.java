959c9e15b1440832db9d5df7971d9b01dc6fc557
JavaEnterpriseWarriors <javaenterprisewarriors@gmail.com>
2014-08-14 01:00:40 +0200

    Adding Storage, implementing Product.

--- main/src/main/java/pl/javaenterprisewarriors/shoptemplate/domain/Product.java
+++ main/src/main/java/pl/javaenterprisewarriors/shoptemplate/domain/Product.java
@@ -1,5 +1,55 @@
 package pl.javaenterprisewarriors.shoptemplate.domain;
 
-public class Product {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "PRODUCT_ID")
+	private int product_id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "PRICE")
+	private int price;
+	
+	public Product(){}
+	
+	public Product(String name, int price)
+	{
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
---
+++ main/src/main/java/pl/javaenterprisewarriors/shoptemplate/domain/Storage.java
@@ -0,0 +1,49 @@
+package pl.javaenterprisewarriors.shoptemplate.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "STORAGE")
+public class Storage {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@OneToMany
+	@JoinColumn(name = "PRODUCT_ID")
+	private Collection<Product> products = new ArrayList<Product>();
+	
+	public Storage(){}
+	
+	public Storage(Collection<Product> products)
+	{
+		this.products = products;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Collection<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Collection<Product> products) {
+		this.products = products;
+	}
+}
