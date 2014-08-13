6b2278e39b203ad907d0e7212e8107889291220a
JavaEnterpriseWarriors <javaenterprisewarriors@gmail.com>
2014-08-14 01:14:36 +0200

    Basket implementation

--- main/src/main/java/pl/javaenterprisewarriors/shoptemplate/shoppingcomponent/Basket.java
+++ main/src/main/java/pl/javaenterprisewarriors/shoptemplate/shoppingcomponent/Basket.java
@@ -1,5 +1,23 @@
 package pl.javaenterprisewarriors.shoptemplate.shoppingcomponent;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.javaenterprisewarriors.shoptemplate.domain.Product;
+
 public class Basket {
 
+	private List<Product> productList = new ArrayList<Product>();
+	
+	public void addProductToBasket(Product product) {
+		productList.add(product);
+	}
+	
+	public void removeProductFromBasket(Product product) {
+		productList.remove(product);
+	}
+	
+	public List<Product> getProductList() {
+		return productList;
+	}
 }
